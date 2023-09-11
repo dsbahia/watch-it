0cf43f5db58ee71613b76e323e0bdfeaba8d6d2f
Max Staite <max.staite2004@gmail.com>
2023-09-11 21:21:11 +0100

    SearchBar component

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "../styles/App.css";
+import SearchBar from "./SearchBar";
 
 function App() {
   return (
     <div className="App">
       <p>Hello World!</p>
+      <SearchBar />
     </div>
   );
 }
