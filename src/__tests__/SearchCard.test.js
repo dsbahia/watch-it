945037a895f40d0beaed7169472a288e1957bf2f
Max Staite <max.staite2004@gmail.com>
2023-09-14 15:08:40 +0100

    searchard and search resultscard

---
+++ src/__tests__/SearchCard.test.js
@@ -0,0 +1,3 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import SearchCard from "../components/SearchCard";
--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,20 @@ import React from "react";
 import "../styles/App.css";
 import SearchBar from "./SearchBar";
 import watchItLogo from "../images/watch-it.png";
+import SearchCard from "./SearchCard";
+import results from "../data/MovieData.json";
 
 function App() {
+  const { original_title, poster_path, overview } = results.results[0];
   return (
     <div className="App">
       <img className="watch-it-logo" src={watchItLogo} alt="Watch It Logo" />
       <SearchBar />
+      <SearchCard
+        title={original_title}
+        posterpath={poster_path}
+        overview={overview}
+      />
     </div>
   );
 }
