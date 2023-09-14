7acc613b8efa731c6c6957950133f7616c125054
Max Staite <max.staite2004@gmail.com>
2023-09-14 20:49:00 +0100

    searchcard searchbar

--- src/components/App.js
+++ src/components/App.js
@@ -6,21 +6,15 @@ import UpComing from "./UpComing";
 import Trending from "./Trending";
 import watchItLogo from "../images/watch-it.png";
 import SearchCard from "./SearchCard";
-import results from "../data/MovieData.json";
 import "../styles/App.css";
 
 function App() {
-  const { original_title, poster_path, overview } = results.results[0];
   const [searchResults, setSearchResults] = useState();
   return (
     <div className="App">
       <img className="watch-it-logo" src={watchItLogo} alt="Watch It Logo" />
       <SearchBar setSearchResults={setSearchResults} />
-      <SearchCard
-        title={original_title}
-        posterpath={poster_path}
-        overview={overview}
-      />
+      <SearchCard />
       <NavBar />
       <Routes>
         <Route path="/" element={UpComing} />
---
+++ src/components/Trending.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "../styles/trending.css";
+
+function Trending() {
+  return <div className="trending" />;
+}
+
+export default Trending;
