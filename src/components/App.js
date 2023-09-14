86ecea1acb1c25fab3e59bf18bc0b52256182cdf
Max Staite <max.staite2004@gmail.com>
2023-09-14 20:44:41 +0100

    searchcard searchbar

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,31 @@
-import React from "react";
-import "../styles/App.css";
+import React, { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import SearchBar from "./SearchBar";
+import NavBar from "./NavBar";
+import UpComing from "./UpComing";
+import Trending from "./Trending";
 import watchItLogo from "../images/watch-it.png";
 import SearchCard from "./SearchCard";
 import results from "../data/MovieData.json";
+import "../styles/App.css";
 
 function App() {
   const { original_title, poster_path, overview } = results.results[0];
+  const [searchResults, setSearchResults] = useState();
   return (
     <div className="App">
       <img className="watch-it-logo" src={watchItLogo} alt="Watch It Logo" />
-      <SearchBar />
+      <SearchBar setSearchResults={setSearchResults} />
       <SearchCard
         title={original_title}
         posterpath={poster_path}
         overview={overview}
       />
+      <NavBar />
+      <Routes>
+        <Route path="/" element={UpComing} />
+        <Route path="trending" element={Trending} />
+      </Routes>
     </div>
   );
 }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,25 +1,32 @@
 import React, { useState } from "react";
+import searchMovies from "../api/api";
 import "../styles/searchbar.css";
 
-function SearchBar() {
+function SearchBar({ setSearchResults }) {
   const [value, setValue] = useState();
-  
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    setSearchResults(searchMovies(value));
+    console.log(value);
+  };
+
   return (
     <div className="search-bar">
-      <form className="search-form">
-        <input 
-         className="search-input"
-         type="text"
-         placeholder="Search"
-         onChange={(e) => setValue(e.target.value)} 
+      <form className="search-form" onSubmit={handleSubmit}>
+        <input
+          className="search-input"
+          type="text"
+          placeholder="Search"
+          onChange={(e) => setValue(e.target.value)}
+          data-testid="search-input"
         />
         <button className="search-button" type="submit">
-           Go!
+          Go!
         </button>
       </form>
     </div>
-     
-  )
+  );
 }
 
 export default SearchBar;
