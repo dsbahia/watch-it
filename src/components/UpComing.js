09d0bbb11747f1e0b9b783ad230e1ba1a51bcf87
Jonnie <jonniet23@hotmail.com>
2023-09-16 15:09:11 +0100

    NavBar tests passing

--- src/components/UpComing.js
+++ src/components/UpComing.js
@@ -2,7 +2,7 @@ import React from "react";
 import "../styles/upcoming.css";
 
 function UpComing() {
-  return <div className="upcoming"></div>;
+  return <div className="upcoming" />;
 }
 
 export default UpComing;
