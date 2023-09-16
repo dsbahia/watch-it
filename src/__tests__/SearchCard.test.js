fc4a8d2a7a393453161edf154954c5809ca8a2cb
Max Staite <max.staite2004@gmail.com>
2023-09-16 21:00:25 +0100

    searchCard tests - not done

--- src/__tests__/SearchCard.test.js
+++ src/__tests__/SearchCard.test.js
@@ -1,3 +1,29 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
 import SearchCard from "../components/SearchCard";
+
+describe("SearchCard", () => {
+  const title = "guardians of the galaxy";
+  const posterpath =
+    "https://image.tmdb.org/t/p/w92//r2J02Z2OpNTctfOSN1Ydgii51I3.jpg";
+  const overview = "Peter Quill, still reeling from the loss of Gamora";
+
+  const { asFragment, getByText } = render(
+    <BrowserRouter>
+      <SearchCard title={title} posterpath={posterpath} overview={overview} />,
+    </BrowserRouter>,
+  );
+
+  it("renders  correctly", () => {
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it("displays componenent with props that were provided", () => {
+    expect(getByText(title)).toBeInTheDocument();
+
+    expect(getByText(overview)).toBeInTheDocument();
+
+    expect(getByText(posterpath)).toBeInTheDocument();
+  });
+});
