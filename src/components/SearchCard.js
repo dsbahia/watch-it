2a57d50df55eb6de088c5cf7136a418827f103b9
Max Staite <max.staite2004@gmail.com>
2023-09-17 18:38:56 +0100

    no poster function

--- src/components/SearchCard.js
+++ src/components/SearchCard.js
@@ -3,8 +3,31 @@ import "../styles/searchcard.css";
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faInfoCircle } from "@fortawesome/free-solid-svg-icons";
+import NoImagePlaceholder from "../images/No-Image-Placeholder.svg.png";
 
 function SearchCard({ title, posterpath, overview }) {
+  if (posterpath === null) {
+    return (
+      <div className="search-card">
+        <div className="poster-card">
+          {" "}
+          <img
+            className="poster-img"
+            alt={`${title} Movie poster`}
+            src={NoImagePlaceholder}
+          />
+        </div>
+        <div className="title-card-">{title}</div>
+        <div className="overview-card">{overview}</div>
+        <div className="more-details">
+          <Link className="more-details-link" to="/">
+            More Details
+            <FontAwesomeIcon icon={faInfoCircle} className="icon" />
+          </Link>
+        </div>
+      </div>
+    );
+  }
   return (
     <div className="search-card">
       <div className="poster-card">
