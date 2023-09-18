beab318daf7bd92a461bb9c90d9c9adaf9438c6a
Jonnie <jonniet23@hotmail.com>
2023-09-18 12:09:13 +0100

    additional styling and fixes to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -12,10 +12,15 @@ function Footer() {
         <div className="footer-links">
           <a href="https://www.themoviedb.org/about">About TMDB</a>
           <a href="https://developer.themoviedb.org/docs">Getting started</a>
-          <a href="https://github.com/dsbahia/watch-it">Developers Github</a>
+          <a href="https://github.com/dsbahia/watch-it">Design Team Github</a>
         </div>
       </div>
-      <p className="tmdb-info">Thank you to The Movie Database for the API</p>
+      <p className="tmdb-info">
+        Acknowledgements to The Movie Database for the use of the API
+      </p>
+      <p className="team-info">
+        Design Team: Dal Bahia, Max Staite and Jonathan Thompson
+      </p>
     </footer>
   );
 }
