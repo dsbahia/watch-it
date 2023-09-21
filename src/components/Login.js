c2c8e6306b60cf1ff4e5d70f7b5a062fa2248b39
Jonnie <jonniet23@hotmail.com>
2023-09-21 23:01:31 +0100

    initial setup of form using state

---
+++ src/components/Login.js
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+const handleSubmit = async (e) =>
+
+
+
+
+  return <div></div>;
+}
+
+export default Login;
