7f3faf93514887e644d6effeb2b97b9e43bcc696
dsbahia <129111782+dsbahia@users.noreply.github.com>
2023-09-25 20:58:28 +0100

    Fixed bugs in navbar when we added dropdown menus

--- src/components/DropDowns/MovieDropDown.js
+++ src/components/DropDowns/MovieDropDown.js
@@ -4,15 +4,7 @@ import DropdownButton from "react-bootstrap/DropdownButton";
 
 function MovieDropDown() {
   return (
-    <DropdownButton
-      id="movie-dropdown"
-      title="Movies"
-      drop="down-centered"
-      autoClose="inside"
-      data-bs-theme="dark"
-      variant="secondary"
-      class="bg-transparent"
-    >
+    <DropdownButton id="item-movie-dropdown" title="Movies">
       <Dropdown.Item href="/top-rated-movies">Top Rated</Dropdown.Item>
       <Dropdown.Item href="/upcoming-movies">Upcoming</Dropdown.Item>
     </DropdownButton>
