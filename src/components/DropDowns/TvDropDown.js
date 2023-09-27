eff1a383a745aab622966946303aaa6527052d0b
Max Staite <max.staite2004@gmail.com>
2023-09-27 17:38:11 +0100

    dropdown tests

--- src/components/DropDowns/TvDropDown.js
+++ src/components/DropDowns/TvDropDown.js
@@ -5,8 +5,12 @@ import DropdownButton from "react-bootstrap/DropdownButton";
 function TvDropDown() {
   return (
     <DropdownButton id="item-tv-dropdown" title="Tv Shows">
-      <Dropdown.Item href="/top-rated-tv-shows">Top Rated</Dropdown.Item>
-      <Dropdown.Item href="/airing-tv-shows">Airing</Dropdown.Item>
+      <Dropdown.Item data-testid="top-rated-link" href="/top-rated-tv-shows">
+        Top Rated
+      </Dropdown.Item>
+      <Dropdown.Item data-testid="airing-link" href="/airing-tv-shows">
+        Airing
+      </Dropdown.Item>
     </DropdownButton>
   );
 }
