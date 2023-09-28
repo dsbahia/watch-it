e07b91fa51a11ed3f59326d983f2a054b6e4b18d
Jonnie <jonniet23@hotmail.com>
2023-09-28 15:42:13 +0100

    verifyemail works with countdown timer and navigates to homepage

--- src/components/Registration/VerifyEmail.js
+++ src/components/Registration/VerifyEmail.js
@@ -1,32 +1,46 @@
 import { useState, useEffect } from "react";
-import { sendEmailVerification } from "firebase/auth";
 import { auth } from "./firebase";
 import { useNavigate } from "react-router-dom";
+import { sendEmailVerification } from "firebase/auth";
 import { useAuthValue } from "./AuthContext";
 import "../../styles/verifyEmail.css";
 
 function VerifyEmail() {
   const { currentUser } = useAuthValue();
-  const [time, setTime] = useState([]);
-  const [endTime, setEndTime] = useState([]);
+  const [time, setTime] = useState(60);
   const { timeActive, setTimeActive } = useAuthValue();
   const navigate = useNavigate();
 
-  const startWork = (e) => {
-    e.preventDefault();
-    const StartTime = {
-      thisTime: new Date().toLocaleString(),
-    };
-    setTime(StartTime);
-  };
+  useEffect(() => {
+    const interval = setInterval(() => {
+      currentUser
+        ?.reload()
+        .then(() => {
+          if (currentUser?.emailVerified) {
+            clearInterval(interval);
+            navigate("/");
+          }
+        })
+        .catch((err) => {
+          alert(err.message);
+        });
+    }, 1000);
+  }, [navigate, currentUser]);
 
-  const finishWork = (e) => {
-    e.preventDefault();
-    const StopTime = {
-      endTime: new Date().toLocaleString(),
-    };
-    setEndTime(StopTime);
-  };
+  useEffect(() => {
+    let interval = null;
+    if (timeActive && time !== 0) {
+      interval = setInterval(() => {
+        setTime((time) => time - 1);
+      }, 1000);
+    } else if (time === 0) {
+      setTimeActive(false);
+      setTime(60);
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [timeActive, time, setTimeActive]);
+
   const resendEmailVerification = () => {
     sendEmailVerification(auth.currentUser)
       .then(() => {
@@ -36,6 +50,7 @@ function VerifyEmail() {
         alert(err.message);
       });
   };
+
   return (
     <div className="center">
       <div className="verify-email">
@@ -46,11 +61,7 @@ function VerifyEmail() {
           <span>{currentUser?.email}</span>
         </p>
         <span>Follow the instruction in the email to verify your account</span>
-        <button
-          type="button"
-          onClick={resendEmailVerification}
-          disabled={endTime.endTime}
-        >
+        <button onClick={resendEmailVerification} disabled={timeActive}>
           Resend Email
           {timeActive && time}
         </button>
--- src/components/Registration/VerifyEmailNew.js
+++ src/components/Registration/VerifyEmailNew.js
@@ -1,4 +1,79 @@
 // import { useState, useEffect } from "react";
+// import { auth } from "./firebase";
+// import { useNavigate } from "react-router-dom";
+// import { sendEmailVerification } from "firebase/auth";
+// import { useAuthValue } from "./AuthContext";
+// import "./verifyEmail.css";
+
+// function VerifyEmail() {
+//   const { currentUser } = useAuthValue();
+//   // const [buttonDisabled, setButtonDisabled] = useState(false);
+//   const [time, setTime] = useState(60);
+//   const { timeActive, setTimeActive } = useAuthValue();
+//   const navigate = useNavigate();
+
+//   useEffect(() => {
+//     const interval = setInterval(() => {
+//       currentUser
+//         ?.reload()
+//         .then(() => {
+//           if (currentUser?.emailVerified) {
+//             clearInterval(interval);
+//             navigate("/");
+//           }
+//         })
+//         .catch((err) => {
+//           alert(err.message);
+//         });
+//     }, 1000);
+//   }, [navigate, currentUser]);
+
+//   useEffect(() => {
+//     let interval = null;
+//     if (timeActive && time !== 0) {
+//       interval = setInterval(() => {
+//         setTime((time) => time - 1);
+//       }, 1000);
+//     } else if (time === 0) {
+//       setTimeActive(false);
+//       setTime(60);
+//       clearInterval(interval);
+//     }
+//     return () => clearInterval(interval);
+//   }, [timeActive, time, setTimeActive]);
+
+//   const resendEmailVerification = () => {
+//     sendEmailVerification(auth.currentUser)
+//       .then(() => {
+//         setTimeActive(true);
+//       })
+//       .catch((err) => {
+//         alert(err.message);
+//       });
+//   };
+
+//   return (
+//     <div className="center">
+//       <div className="verify-email">
+//         <h1>Verify your Email Address</h1>
+//         <p>
+//           <strong>A Verification email has been sent to:</strong>
+//           <br />
+//           <span>{currentUser?.email}</span>
+//         </p>
+//         <span>Follow the instruction in the email to verify your account</span>
+//         <button onClick={resendEmailVerification} disabled={timeActive}>
+//           Resend Email
+//           {timeActive && time}
+//         </button>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default VerifyEmail;
+
+// import { useState, useEffect } from "react";
 // import { sendEmailVerification } from "firebase/auth";
 // import { auth } from "./firebase";
 // import { useNavigate } from "react-router-dom";
@@ -58,4 +133,5 @@
 //     </div>
 //   );
 // }
+
 // export default VerifyEmail;
