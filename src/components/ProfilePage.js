a6328425a70b693368e9de4c0af86cbd033e9cfc
Max Staite <max.staite2004@gmail.com>
2023-09-30 17:07:21 +0100

    added routing back to homepage when sign out button clicked

--- src/components/ProfilePage.js
+++ src/components/ProfilePage.js
@@ -1,10 +1,21 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { signOut } from "firebase/auth";
 import { useAuthValue } from "./Registration/AuthContext";
 import { auth } from "./Registration/firebase";
 
 function Profile() {
   const { currentUser } = useAuthValue();
+  const navigate = useNavigate();
+
+  const handleSignOut = async () => {
+    try {
+      await signOut(auth);
+      navigate("/"); // Redirect to the "/" route after signing out
+    } catch (error) {
+      console.error("Sign out error", error);
+    }
+  };
   return (
     <div className="center">
       <div className="profile">
@@ -17,7 +28,7 @@ function Profile() {
           <strong>Email verified: </strong>
           {`${currentUser?.emailVerified}`}{" "}
         </p>
-        <button type="button" onClick={() => signOut(auth)}>
+        <button type="button" onClick={handleSignOut}>
           Sign Out
         </button>
       </div>
